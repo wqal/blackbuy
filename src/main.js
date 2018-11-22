cdae3ae8462b1785523d5d77d9471aca5d0f5404
wqal <664652651@qq.com>
2018-11-22 20:50:17 +0800

    2018年11月22日20:50:08 提交第一天

--- src/main.js
+++ src/main.js
@@ -3,6 +3,32 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 全局样式
+import './assets/site/css/style.css'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter);
+
+
+import index from './components/index.vue'
+
+let routes = [{
+    path: '/',
+    // 重定向
+    redirect: '/index'
+  },
+  {
+    path: '/index',
+    component: index
+  }
+]
+
+// 实例化路由对象
+let router = new VueRouter({
+  routes
+})
+
 new Vue({
   render: h => h(App),
+
+  router
 }).$mount('#app')
